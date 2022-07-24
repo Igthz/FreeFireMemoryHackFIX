eed1e88eccea7a61b80acf80343b5d5d7d5f7b72
igth <83485103+Igthz@users.noreply.github.com>
2022-07-23 23:11:54 -0700

    Add files via upload

---
+++ FreeFireMemoryHack/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace FreeFireMemoryHack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.NORECOIL = new Guna.UI2.WinForms.Guna2Button();
+            this.PID = new System.Windows.Forms.Label();
+            this.PSPS = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guna2Button4
+            // 
+            this.guna2Button4.BorderRadius = 6;
+            this.guna2Button4.BorderThickness = 1;
+            this.guna2Button4.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button4.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button4.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button4.FillColor = System.Drawing.Color.Red;
+            this.guna2Button4.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button4.ForeColor = System.Drawing.Color.White;
+            this.guna2Button4.Location = new System.Drawing.Point(12, 117);
+            this.guna2Button4.Name = "guna2Button4";
+            this.guna2Button4.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button4.TabIndex = 3;
+            this.guna2Button4.Text = "VERMELHAO";
+            this.guna2Button4.Click += new System.EventHandler(this.guna2Button4_Click);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.BorderRadius = 6;
+            this.guna2Button3.BorderThickness = 1;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.Red;
+            this.guna2Button3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button3.ForeColor = System.Drawing.Color.White;
+            this.guna2Button3.Location = new System.Drawing.Point(199, 117);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button3.TabIndex = 2;
+            this.guna2Button3.Text = "AIMFOV";
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button3_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderRadius = 6;
+            this.guna2Button2.BorderThickness = 1;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.Red;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(199, 55);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button2.TabIndex = 1;
+            this.guna2Button2.Text = "AIMBOT";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // NORECOIL
+            // 
+            this.NORECOIL.BorderRadius = 6;
+            this.NORECOIL.BorderThickness = 1;
+            this.NORECOIL.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.NORECOIL.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.NORECOIL.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.NORECOIL.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.NORECOIL.FillColor = System.Drawing.Color.Red;
+            this.NORECOIL.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.NORECOIL.ForeColor = System.Drawing.Color.White;
+            this.NORECOIL.Location = new System.Drawing.Point(392, 117);
+            this.NORECOIL.Name = "NORECOIL";
+            this.NORECOIL.Size = new System.Drawing.Size(180, 45);
+            this.NORECOIL.TabIndex = 0;
+            this.NORECOIL.Text = "NORECOIL";
+            this.NORECOIL.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // PID
+            // 
+            this.PID.AutoSize = true;
+            this.PID.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.PID.Location = new System.Drawing.Point(535, 87);
+            this.PID.Name = "PID";
+            this.PID.Size = new System.Drawing.Size(25, 13);
+            this.PID.TabIndex = 5;
+            this.PID.Text = "PID";
+            // 
+            // PSPS
+            // 
+            this.PSPS.AutoSize = true;
+            this.PSPS.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.PSPS.Location = new System.Drawing.Point(12, 87);
+            this.PSPS.Name = "PSPS";
+            this.PSPS.Size = new System.Drawing.Size(126, 13);
+            this.PSPS.TabIndex = 6;
+            this.PSPS.Text = "waiting for connection.....";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(554, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(584, 181);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PSPS);
+            this.Controls.Add(this.PID);
+            this.Controls.Add(this.guna2Button4);
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.NORECOIL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label PID;
+        private System.Windows.Forms.Label PSPS;
+        private Guna.UI2.WinForms.Guna2Button NORECOIL;
+        private Guna.UI2.WinForms.Guna2Button guna2Button4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FreeFireMemoryHack/Form1.cs
@@ -0,0 +1,220 @@
+﻿using Memory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FreeFireMemoryHack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("KERNEL32.DLL")]
+        public static extern IntPtr CreateToolhelp32Snapshot(uint flags, uint processid);
+        [DllImport("KERNEL32.DLL")]
+        public static extern int Process32First(IntPtr handle, ref ProcessEntry32 pe);
+        [DllImport("KERNEL32.DLL")]
+        public static extern int Process32Next(IntPtr handle, ref ProcessEntry32 pe);
+
+        private async Task PutTaskDelay(int Time)
+        {
+            await Task.Delay(Time);
+        }
+        private async Task<IntPtr> GlockRajaadakk(string type)
+        {
+            string Bluestacks = "HD-Player";
+
+            var intPtr = IntPtr.Zero;
+            uint num = 0U;
+            var intPtr2 = CreateToolhelp32Snapshot(2U, 0U);
+            if ((int)intPtr2 > 0)
+            {
+                ProcessEntry32 processEntry = default;
+                processEntry.dwSize = (uint)Marshal.SizeOf(processEntry);
+                int num2 = Process32First(intPtr2, ref processEntry);
+                while (num2 == 1)
+                {
+                    var intPtr3 = Marshal.AllocHGlobal((int)processEntry.dwSize);
+                    Marshal.StructureToPtr(processEntry, intPtr3, true);
+                    ProcessEntry32 processEntry2 = (ProcessEntry32)Marshal.PtrToStructure(intPtr3, typeof(ProcessEntry32));
+                    Marshal.FreeHGlobal(intPtr3);
+                    if (processEntry2.szExeFile.Contains(Bluestacks) && processEntry2.cntThreads > num)
+                    {
+                        num = processEntry2.cntThreads;
+                        intPtr = (IntPtr)(long)(ulong)processEntry2.th32ProcessID;
+                    }
+
+                    num2 = Process32Next(intPtr2, ref processEntry);
+                }
+                PID.Text = Convert.ToString(intPtr);
+                await PutTaskDelay(1000);
+
+                if (type == "NORECOIL")
+                {
+                    changeMEMORY("ODAgMzcgOEYgM0M=", "MDAgRDcgNUIgMDA=");
+                }
+                else if (type == "NORECOILDESATIVAR")
+                {
+                    changeMEMORY("MDAgRDcgNUIgMDA=", "ODAgMzcgOEYgM0M=");
+                }
+                else if (type == "AIMBOT")
+                {
+                    changeMEMORY("NjIgNmYgNmUgNjUgNWYgNDggNjkgNzAgNzM=", "NjIgNmYgNmUgNjUgNWYgNDggNjUgNjEgNjQ=");
+                }
+                else if (type == "AIMFOV")
+                {
+                    changeMEMORY("QUUgNDcgMDEgM0Y=", "ODAgN0IgRTEgRkYgRkYgRkYgRkYgRkY=");
+                }
+                else if (type == "VERMELHAO")
+                {
+                    changeMEMORY("OTYgMDAgMDAgMDAgMDAgMDAgQjAgNDAgMDAgMDAgODAgM0YgMDAgMDAgNDAgM0Y=", "OTYgMDAgMDAgMDAgMDAgMDAgQjggNDAgMDAgMDAgMDAgMDAgMDAgMDAgMDAgMDA=");
+                }
+            }
+            return intPtr;
+        }
+
+
+        private string sr;
+
+        public async void DRIERSSSS_LOAD_AUTO_IN_PROCESSS(string type)
+        {
+            Console.Beep(420, 250);
+            x = 0;
+            await GlockRajaadakk(type);
+        }
+        public long enumerable = new long();
+
+
+        public async void changeMEMORY(string search, string replace)
+        {
+            bool k = false;
+            int counter = 1;
+            if (Convert.ToInt32(PID.Text) == 0)
+            {
+                PSPS.ForeColor = Color.Red;
+                PSPS.Text = "Falha ao Conectar.";
+                Console.Beep(1250, 250);
+            }
+            else
+            {
+                PSPS.ForeColor = Color.Green;
+                PSPS.Text = "Conectado.";
+                MemLib.OpenProcess(Convert.ToInt32(PID.Text));
+
+                Thread.Sleep(2000);
+
+                PSPS.ForeColor = Color.Orange;
+                PSPS.Text = "Aplicando...";
+
+                var enumerable = await MemLib.AoBScan(67108864L, 4294967295L, infile(search), true, true, string.Empty);
+                string_0 = "0x" + enumerable.FirstOrDefault().ToString("X");
+                Mem.MemoryProtection memoryProtection;
+                MemLib.ChangeProtection(string_0, Mem.MemoryProtection.ReadWrite, out memoryProtection, "");
+
+                foreach (long num in enumerable)
+                {
+                    MemLib.WriteMemory(num.ToString("X"), "bytes", infile(replace), string.Empty, null);
+                    k = true;
+                }
+
+                if (k == true)
+                {
+                    Console.Beep(1250, 250);
+                    PSPS.Text = "Aplicado!";
+                    PSPS.ForeColor = Color.Green;
+                    await PutTaskDelay(500);
+                }
+                else if (counter < 4)
+                {
+                    Console.Beep(1250, 250);
+                    PSPS.ForeColor = Color.Red;
+                    PSPS.Text = "Falha ao Aplicar";
+                    counter += 1;
+                }
+                else
+                {
+                    Console.Beep(1250, 250);
+                    PSPS.Text = "Falha ao Aplicar";
+                    PSPS.ForeColor = Color.Red;
+                }
+
+                Mem.MemoryProtection memoryProtection2;
+                MemLib.ChangeProtection(string_0, Mem.MemoryProtection.ReadOnly, out memoryProtection2, "");
+
+            }
+        }
+
+        public static string infile(string value)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(value));
+        }
+
+        private int x;
+        public Mem MemLib = new Mem();
+        private static string string_0;
+        private IContainer icontainer_0;
+
+        public struct ProcessEntry32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ProcessID;
+            public IntPtr th32DefaultHeapID;
+            public uint th32ModuleID;
+            public uint cntThreads;
+            public uint th32ParentProcessID;
+            public int pcPriClassBase;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szExeFile;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            PSPS.Text = "Conectando...";
+            PSPS.ForeColor = Color.Orange;
+            DRIERSSSS_LOAD_AUTO_IN_PROCESSS("NORECOIL");
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            PSPS.Text = "Conectando...";
+            PSPS.ForeColor = Color.Orange;
+            DRIERSSSS_LOAD_AUTO_IN_PROCESSS("AIMBOT");
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            PSPS.Text = "Conectando...";
+            PSPS.ForeColor = Color.Orange;
+            DRIERSSSS_LOAD_AUTO_IN_PROCESSS("AIMFOV");
+        }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+            PSPS.Text = "Conectando...";
+            PSPS.ForeColor = Color.Orange;
+            DRIERSSSS_LOAD_AUTO_IN_PROCESSS("VERMELHAO");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
+//by igth
